a3256c66b5d4002e6f5738b24c8633f35716dbc4
DESKTOP-EQM75TJ\quang <quanghuynh1206@gmail.com>
2021-01-05 00:03:18 +0900

    frontend
    lan1

---
+++ frontendNJS/layouts/admin_login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <!-- header -->
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <!-- content -->
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <!-- footer -->
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      fixed: false,
+      title: 'Laravel Nuxt Admin'
+    }
+  }
+}
+</script>
