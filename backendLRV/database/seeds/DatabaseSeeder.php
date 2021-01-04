92ead704f6e3df93aa3a0f3f20a23ce41949a010
DESKTOP-EQM75TJ\quang <quanghuynh1206@gmail.com>
2021-01-05 00:00:45 +0900

    lan1
    push code lan 1

---
+++ backendLRV/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+        	'name'=>'King',
+        	'email'=>'king@gmail.com',
+        	'password'=>Hash::make('123')	
+        ]);
+    }
+}
